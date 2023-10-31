07544b337e809730cb3e8760f620d0fcddbc3316
Jimmy <toluadejimi@gmail.com>
2023-10-31 07:54:30 +0100

    pushhhh

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -134,7 +134,7 @@ class ProductController extends Controller
         $data['strem_items'] = Item::where('cat_id', 18)->take(5)->get();
 
 
-        return redirect('welcome');
+        return redirect('welcome', $data);
 
 
     }
